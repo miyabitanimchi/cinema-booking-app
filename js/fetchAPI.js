1719120cddb68599b588f10011e424e8ac2020b1
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-04-26 18:41:48 -0700

    add images etc

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -2,28 +2,14 @@
 
 const API_KEY = "6de0031ff9ff7891e34f8d09f9cbc39b";
 
-
-// const fetchAPI = () => {
-//   fetch(`https://api.themoviedb.org/3/movie/550?api_key=${api}`).then((response) => {
-//     console.log(response);
-//     if (!response.ok) {
-//       alert("Oops, there is an error");
-//       return;
-//     }
-//     response.json().then((data) => {
-//       console.log(data);
-//       console.log(data.original_title);
-//     })
-//   });
-// }
-
 const fetchAPI = async () => {
   try {
     const response = await axios.get(`https://api.themoviedb.org/4/list/1?api_key=${API_KEY}`);
     const movieData = response.data;
     console.log(movieData);
     showTitle(movieData);
-    showImg(movieData);
+    // showImg(movieData);
+    showMovieContents(movieData);
   } 
   catch(errors) {
     console.log(`Oops, errors! ${errors}`);
@@ -42,4 +28,14 @@ const showImg = (data) => {
   const imgElement2 = document.createElement("img");
   document.getElementById("testDiv2").appendChild(imgElement2);
   imgElement2.src = `https://image.tmdb.org/t/p/w200/${data.results[0].poster_path}`;
+}
+
+const showMovieContents = (data) => {
+  
+  for (i = 0; i < data.results.length; i++) {
+    const imgElement2 = document.createElement("img");
+    document.getElementById("movie-collection").appendChild(imgElement2);
+    imgElement2.src = `https://image.tmdb.org/t/p/w200/${data.results[i].poster_path}`;
+    console.log(imgElement2.src);
+  }
 }
