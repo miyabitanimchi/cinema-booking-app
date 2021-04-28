1c4a1e5ef62b25a3114a23c35a1cc768c1536749
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-04-28 12:51:51 -0700

    add top title etc

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,47 +1,38 @@
 const API_KEY = "6de0031ff9ff7891e34f8d09f9cbc39b";
 
+
+
 const fetchAPI = async () => {
   try {
     const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`);
-    const response2 = await axios.get(`https://api.themoviedb.org/3/movie/460465/videos?api_key=${API_KEY}&language=en-US`);
+    // const response2 = await axios.get(`https://api.themoviedb.org/3/movie/460465/videos?api_key=${API_KEY}&language=en-US`);
+    const response3 = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
 
+    console.log(response3.data);
     const movieData = response.data;
-    const videoData = response2.data.results[1].key;
+    // const videoData = response2.data.results[1].key;
 
-    // document.getElementById("testIframe").src = `https://www.youtube.com/embed/${videoData}?modestbranding=1&rel=0&showinfo=0&autoplay=1&mute=1&playsinline=1&loop=1&playlist=${videoData}&controls=0&disablekb=1`;
     console.log(movieData);
-    console.log(videoData);
-    showMovieContents(movieData);
-    appendImgAndInfo(movieData);
-    
+    // console.log(videoData);
+    foundationForNowPlaying(movieData);
+    appendImgAndInfoFprNowPlaying(movieData);
+    foundationForMostPopular(movieData);
   } 
   catch(errors) {
     console.log(`Oops, errors! ${errors}`);
   }
 }
 
-
-// const showTopInfo = (data) => {
-//   const elForTopImg = document.querySelectorAll(".topimage");
-
-//   for (i = 0; i < elForTopImg.length; i++) {
-//     const randomNum = Math.floor(Math.random() * elForTopImg.length);
-//     console.log(randomNum);
-//   }
-// }
-// showTopInfo();
-
-const showMovieContents = (data) => {
-  
+const foundationForNowPlaying = (data) => {
   for (let i = 0; i < data.results.length; i++) {
-    const divElement = document.createElement("div");
-    divElement.classList.add("position-for-img-and-caption");
-    document.getElementById("movie-collection").appendChild(divElement);
-    console.log(divElement);
+    const divElementForMostPopular = document.createElement("div");
+    divElementForMostPopular.classList.add("position-for-img-and-caption");
+    document.getElementById("movie-collection-now-playing").appendChild(divElementForMostPopular);
+    console.log(divElementForMostPopular);
   }
 }
 
-const appendImgAndInfo = (data) => {
+const appendImgAndInfoFprNowPlaying = (data) => {
   const el = document.querySelectorAll(".position-for-img-and-caption");
   console.log(el);
 
@@ -58,17 +49,29 @@ const appendImgAndInfo = (data) => {
     el[i].appendChild(imgElement);
     imgElement.src = `https://image.tmdb.org/t/p/w200/${data.results[i].poster_path}`;
     el[i].appendChild(pElement);
-    pElement.innerText = data.results[i].original_title;
+    pElement.innerText = data.results[i].title;
     // el[i].appendChild(overviewElement);
     // overviewElement.innerText = data.results[i].overview;
     console.log(el[i]);
   }
 
   const elForTopImg = document.querySelectorAll(".topimage");
+  const elForTopTitle = document.querySelectorAll(".toptitle");
+  let randomNum;
   for (let i = 0; i < elForTopImg.length; i++) {
-    let randomNum = Math.floor(Math.random() * el.length);
+    randomNum = Math.floor(Math.random() * el.length);
     console.log(randomNum);
     elForTopImg[i].src = `https://image.tmdb.org/t/p/w500/${data.results[randomNum].backdrop_path}`
+    elForTopTitle[i].innerText = data.results[randomNum].title;
+  }
+}
+
+const foundationForMostPopular = (data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    const divElementForMostPopular = document.createElement("div");
+    divElementForMostPopular.classList.add("position-for-img-and-caption");
+    document.getElementById("movie-collection-popular").appendChild(divElementForMostPopular);
+    console.log(divElementForMostPopular);
   }
 }
 
